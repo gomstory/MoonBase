5e418a08f238b89a013a44c6c517f2319d2a9d54
Arkom Madaehoh <arkom.madaehoh@ibm.com>
2021-05-08 06:23:09 +0700

    delete all

--- src/redux/action-types.js
+++
@@ -1,3 +0,0 @@
-export const BUY_MOON = "BUY";
-export const UPDATE_MOON_INFO = "UPDATE_MOON_INFO";
-export const UPDATE_USER = "UPDATE_USER";
--- src/redux/store.js
+++
@@ -1,4 +0,0 @@
-import { createStore } from "redux";
-import Reducer from "./reducer";
-
-export default createStore(Reducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
